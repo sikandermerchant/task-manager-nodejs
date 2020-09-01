9908c08e3f0b9b7e9231e272733ae85b32994aa1
sikandermerchant <sikander.merchant@gmail.com>
2020-09-01 16:48:30 +0100

    Playground for delete in mongodb

--- mongodb.js
+++ mongodb.js
@@ -4,7 +4,10 @@
 // const MongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectID
 
-const { MongoClient, ObjectID } = require("mongodb"); //shorthand of the above 3 declarations
+const {
+  MongoClient,
+  ObjectID
+} = require("mongodb"); //shorthand of the above 3 declarations
 
 // Connection URL
 const connectionURL = "mongodb://127.0.0.1:27017"; ///as per the documentation we could have used localhost instead of IP address 127.0.0.1, but this has evidences of causing issues and also slowing down the application, so recommended to use the IP address
@@ -19,8 +22,10 @@ const databaseName = "task-manager";
 // console.log(id.getTimestamp());
 
 MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
   (error, client) => {
     if (error) {
       return console.log("Unable to connect to Database"); ///return is used so that the function stops after console logging this error message and doesn't go beyond this point, incase there is an error
@@ -28,41 +33,22 @@ MongoClient.connect(
     console.log("Connected Correctly");
 
     const db = client.db(databaseName);
-    db.collection("tasks")
-      // .updateOne(
-      //   {
-      //     _id: new ObjectID("5f3fd8ee56a9f31fd1bb6fd1"),
-      //   },
-      //   {
-      //     // $set: {
-      //     //   name: "Shamim Merchant",
-      //     // },
-      //     $inc: {
-      //       age: 1,
-      //     },
-      //   }
-      // )
-      // .then((result) => {
-      //   console.log(result);
-      // })
-      // .catch((error) => {
-      //   console.log(error);
-      // });
-      .updateMany(
-        {
-          completed: { $eq: false },
-        },
-        {
-          $set: {
-            completed: true,
-          },
-        }
-      )
-      .then((result) => {
-        console.log(result);
+    // db.collection("users").deleteMany({
+    //     age: 41
+    //   }).then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
+
+    db.collection('tasks').deleteOne({
+        description: 'Order new Phone'
+      }).then((result) => {
+        console.log(result.deletedCount);
       })
       .catch((error) => {
         console.log(error);
-      });
+      })
   }
 );
